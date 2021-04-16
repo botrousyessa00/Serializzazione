35fc1397cae2b98659290b2413e0cd959efb43fe
yessabotrous <yessabotrous@localhost>
2021-04-16 21:01:29 +0200

    Serializzazionee

---
+++ src/serializzazione/Studente.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializzazione;
+
+/**
+ *
+ * @author yessabotrous
+ */
+public class Studente {
+
+    private String nome;
+    private String cognome;
+    private int numRegistro;
+
+    public Studente(String nome, String cognome, int numRegistro) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numRegistro = numRegistro;
+    }
+
+}
